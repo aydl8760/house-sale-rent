8102d7aab6307fa0bdca7e994ca87bbef38f045e
Lidya Simon <lidyasimon21@gmail.com>
2025-03-27 04:43:02 +0300

    creating a middleware and user signup controller

---
+++ backend/controllers/user.js
@@ -0,0 +1,39 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.js";
+
+export const signup = async (req, res, next) => {
+  const { userName, email, password } = req.body;
+
+  try {
+    if (!userName || !email || !password) {
+      return res.json({
+        success: false,
+        message: "required",
+      });
+    }
+
+    const createNew = await User.findOne({ email });
+    if (createNew) {
+      return res.json({
+        success: false,
+        message: "Email is already used please try another!",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newlyCreated = new User({
+      userName,
+      email,
+      password: hashedPassword,
+    });
+    await newlyCreated.save();
+    res.status(200).json({
+      success: true,
+      message: "user Registered successfully",
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: "Ops something went wrong, please try again!",
+    });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,19 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/user", userRouter);
 
 app.listen(3050, () => {
   console.log("Server is running on Port 3050");
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.json({
+    success: false,
+    message,
+  });
 });
